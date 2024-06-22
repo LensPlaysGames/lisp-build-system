b6b142059919fe16b0323c01f445c2dad01da78d
Lens_r <lensplaysgames@gmail.com>
2024-06-21 21:32:09 -0700

    Read Source from File
    Kind of nice when it's not just hard-coded, eh?

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,40 @@
 #include <string>
+#include <filesystem>
+#include <cstdio>
 
 #include <parser/parser.h>
 
+auto get_file_contents_or_exit(const std::string_view path) -> std::string {
+    auto f = fopen(path.data(), "rb");
+    if (not f) {
+        printf("ERROR: Cannot get contents of file at %s\n", path.data());
+        exit(1);
+    }
+
+    // Get file size.
+    fseek(f, 0, SEEK_END); // seek to end of file
+    long file_size = ftell(f);
+    fseek(f, 0, SEEK_SET); // seek to beginning of file
+
+    // Allocate string big enough to hold file contents.
+    std::string contents;
+    contents.resize(file_size);
+
+    // Read data into string.
+    fread(contents.data(), file_size, 1, f);
+    fclose(f);
+
+    return contents;
+}
+
 int main() {
-    //std::string source{"(executable foo) (sources foo src/main.cpp) (command foo echo suck it lol)"};
-    std::string source{"(library libparser)(sources libparser lib/parser/parser.cpp)  (executable lbs)(sources lbs src/main.cpp)(include-directories lbs inc)(dependency lbs libparser)"};
+    const char* path = ".lbs";
+    if (not std::filesystem::exists(path)) {
+        printf("No build file at .lbs found, exiting\n");
+        printf("    To learn how to write one, see https://github.com/LensPlaysGames/lisp-build-system\n");
+        return 1;
+    }
+    std::string source = get_file_contents_or_exit(path);
     parse(source);
     return 0;
 }
